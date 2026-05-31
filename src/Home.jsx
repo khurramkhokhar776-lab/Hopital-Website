c151239ee496f6eb831249eaa23243bcc7a27fe6
khurramkhokhar776-lab <khurramkhokhar776@gmail.com>
2026-05-31 20:52:51 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -145,7 +145,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/contact", {
+    const response = await fetch("https://hopital-website.onrender.com/appointment") {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
